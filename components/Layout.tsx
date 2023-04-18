b1de2d64db5934381595be7a5fd60a6047b650cf
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-04-18 13:28:39 +0530

    feat : pop ups and more layout refreshes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,11 +13,10 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
                 <div className='grid grid-cols-4 h-full'>
                     <Sidebar />
                     <div className='col-span-3
-                    lg:col-span-3
+                    lg:col-span-2
                     border-x-[1px]
                     border-neutral-800
                     '>
-
                     {children}
                     </div>
                     <FollowBar/>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import Layout from '@/components/Layout'
 import React from "react";
+import Model from "@/components/Model";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Model actionLabel='Submit' isOpen title='testModel'/>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
