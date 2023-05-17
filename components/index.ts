733d4ef4b6b7ad169a920115505f91238da8aa42
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-05-18 01:40:06 +0530

    refactor: folder names as modules

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+export {default as Avatar} from './Avatar';
+export {default as Button} from './Button';
+export {default as Header} from './Header';
+export {default as Layout} from './Layout';
+export {default as Modal} from './Modal';
+export {default as Input} from './Input';
---
+++ hooks/index.ts
@@ -0,0 +1,6 @@
+export { default as useCurrentUser} from './useCurrentUser';
+export {default as useEditModal} from './useEditModal';
+export {default as useLoginModal} from './useLoginModal';
+export {default as useRegisterModal} from './useRegisterModal';
+export {default as useUser} from './useUser';
+export {default as useUsers} from './useUsers';
