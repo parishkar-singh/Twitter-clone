0bce05460e934e4d0cbcd98ca571e3c7ee853309
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-05-19 19:22:12 +0530

    feat: Feel free to follow anyone

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,7 +1,9 @@
-export { default as useCurrentUser} from './useCurrentUser';
+export {default as useCurrentUser} from './useCurrentUser';
 export {default as useEditModal} from './useEditModal';
 export {default as useLoginModal} from './useLoginModal';
 export {default as useRegisterModal} from './useRegisterModal';
 export {default as useUser} from './useUser';
 export {default as useUsers} from './useUsers';
 export {default as usePosts} from './usePosts';
+export {default as useFollow} from './useFollow';
+export {default as usePost} from './usePost';
